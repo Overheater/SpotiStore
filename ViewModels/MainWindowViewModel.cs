b85a47abd60cc6a3ad50f823a54bf087fdbdc1ec
Overheater <ianpougher@gmail.com>
2023-02-20 16:51:28 -0900

    adds basic UI Updates to the app and async calls

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,23 +1,61 @@
-﻿using SkiaSharp;
+﻿using ReactiveUI;
+using SkiaSharp;
 using SpotiStore.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SpotiStore.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
     {
 
+
+        string playlistID;
+        string playlistName;
+
         public ICommand Command { get; set; }
+        public string PlaylistID
+        {
+            get => playlistID;
+            set =>this.RaiseAndSetIfChanged(ref playlistID, value);
+        }
+        public string PlaylistName
+        {
+            get => playlistName;
+            set
+            {
+                playlistName = value;
+                RaisePropertyChanged();
+            } 
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public async Task<string> QueryPlaylist()
+        {
+            var playlist = await spotifyClient.Playlists.Get(PlaylistID);
+            PlaylistName = playlist.Name;
+            RaisePropertyChanged(playlistName);
+            return playlist.Name;
+        }
+        public void ArchivePlaylist()
+        {
+            return;
+        }
         public MainWindowViewModel()
         {
            createSpotifyClient();
+
         }
         
-        public PlaylistArchiverViewModel Archiver { get; }
-        public string Greeting => "Welcome to Avalonia!";
     }
 }
--- ViewModels/PlaylistFinderViewModel.cs
+++ ViewModels/PlaylistFinderViewModel.cs
@@ -1,24 +1,25 @@
-﻿using SpotiStore.Models;
+﻿using ReactiveUI;
+using SpotiStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SpotiStore.ViewModels
 {
-    public class PlaylistArchiverViewModel :ViewModelBase
+    public class PlaylistFinderViewModel :ViewModelBase
     {
-        public PlaylistArchiverViewModel(API aPI)
+        string playlistID;
+        string PlaylistName { get; set; }
+        public PlaylistFinderViewModel()
         {
-            API = aPI;
         }
-        private API API;
-        private User User;
-        public string UserSearchText = "Search Spotify Users";
+
 
         //public User Get_User(string userId)
         //{
         //    var user = API.GetUser(userId);
         //    return new User();
         //}
+
     }
 }
